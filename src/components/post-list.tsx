b5f940719c0fe12550cd8561f7fa8353f02761e7
R1013-T <nosuke1013@icloud.com>
2024-05-12 02:25:44 +0900

    done

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,33 +1,33 @@
-import { getAllPosts } from "@/server/data/post";
-import { formatDistanceToNow } from "date-fns";
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-} from '@/components/ui/card';
+// import { getAllPosts } from "@/server/data/post";
+// import { formatDistanceToNow } from "date-fns";
+// import {
+//   Card,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+// } from '@/components/ui/card';
 
-export const PostList = async () => {
-  const posts = await getAllPosts();
+// export const PostList = async () => {
+//   const posts = await getAllPosts();
 
-  return (
-    <div className="flex gap-8">
-      {posts.map((post) => (
-        <Card key={post.id}>
-          <CardHeader>
-            <CardDescription className="text-primary">
-              {post.text}
-            </CardDescription>
-          </CardHeader>
-          <CardFooter className="border-t px-6 py-4 text-xs">
-            <p className="text-muted-foreground">
-              {formatDistanceToNow(new Date(post.createdAt), {
-                addSuffix: true,
-              })}
-            </p>
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
-  );
-};
+//   return (
+//     <div className="flex gap-8">
+//       {posts.map((post) => (
+//         <Card key={post.id}>
+//           <CardHeader>
+//             <CardDescription className="text-primary">
+//               {post.text}
+//             </CardDescription>
+//           </CardHeader>
+//           <CardFooter className="border-t px-6 py-4 text-xs">
+//             <p className="text-muted-foreground">
+//               {formatDistanceToNow(new Date(post.createdAt), {
+//                 addSuffix: true,
+//               })}
+//             </p>
+//           </CardFooter>
+//         </Card>
+//       ))}
+//     </div>
+//   );
+// };
--- src/server/actions/post.ts
+++ src/server/actions/post.ts
@@ -1,14 +1,14 @@
-'use server';
+// 'use server';
 
-import { client } from '@/lib/hono';
-import { InferRequestType } from 'hono';
-import { revalidateTag } from 'next/cache';
+// import { client } from '@/lib/hono';
+// import { InferRequestType } from 'hono';
+// import { revalidateTag } from 'next/cache';
 
-const $post = client.api.posts.$post;
-type RequestType = InferRequestType<typeof $post>['form'];
+// const $post = client.api.posts.$post;
+// type RequestType = InferRequestType<typeof $post>['form'];
 
-export const createPost = async (data: RequestType) => {
-  await $post({ form: data });
+// export const createPost = async (data: RequestType) => {
+//   await $post({ form: data });
 
-  revalidateTag('posts');
-};
+//   revalidateTag('posts');
+// };
