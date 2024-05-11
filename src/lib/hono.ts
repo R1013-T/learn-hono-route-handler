0a9ace27b2cf59ded7680c3d70e8edeba0791293
R1013-T <nosuke1013@icloud.com>
2024-05-12 02:41:01 +0900

    done

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -2,8 +2,5 @@ import { hc } from "hono/client";
 
 import { type AppType } from "@/app/api/[[...route]]/route";
 
-const url = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}.vercel.app`
-  : "http://localhost:3000";
 
-export const client = hc<AppType>(url);
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!)
